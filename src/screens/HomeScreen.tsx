9f9c03ec7cc81260db0514ceb5ba59b2979d32d7
Fernando Bernal <fernando.bernal.diaz@gmail.com>
2022-07-16 21:53:43 -0500

    Se configura Vector-Icons

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
-
+import Icon from 'react-native-vector-icons/Ionicons';
 export const HomeScreen = () => {
   return (
-      <View>
+      <View style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
           <Text>HomeScreen</Text>
+          <Icon name="md-home" size={100} color="red" />
       </View>
   )
 }
